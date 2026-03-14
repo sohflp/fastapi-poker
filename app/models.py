1a97973a8dae85069dff436e27f7869e61e681c6
Felipe Rodrigues <sohflp@gmail.com>
2026-03-14 22:53:46 +1100

    Update DB models

--- app/models.py
+++ app/models.py
@@ -8,12 +8,14 @@ class Player(SQLModel, table=True):
 class Game(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     date: str
+    buyin_value: int = 30
+    rebuy_value: int = 25
+    addon_value: int = 25
 
 class PlayerGame(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     player_name: str
     game_id: int
+    position: int | None = None
     rebuys: int = 0
     addons: int = 0
-    position: int | None = None
-    winnings: float = 0

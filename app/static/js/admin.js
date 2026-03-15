5f70ee0bb798ef406b117082e95f7cb780e00877
Felipe Rodrigues <sohflp@gmail.com>
2026-03-15 21:00:13 +1100

    Update style for mobile devices

--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -1,22 +1,54 @@
+function updatePositions() {
+    let rows = document.querySelectorAll("#playersTable tr")
+
+    rows.forEach((row, index) => {
+        row.querySelector(".position").value = index + 1
+    })
+}
+
+
 function addPlayerRow() {
     let table = document.getElementById("playersTable")
-    let row = table.insertRow()
+    let row = document.createElement("tr")
 
     row.innerHTML = `
 <td>
-<input name="position" type="number" class="bg-gray-600 p-3 rounded w-20">
+    <input name="position" class="position bg-gray-800 p-2 rounded w-16 text-center" readonly>
 </td>
+
 <td>
-<input name="player_name" class="bg-gray-600 p-3 w-100 rounded">
+    <input name="player_name" class="bg-gray-600 p-2 rounded w-36 text-center">
 </td>
+
 <td>
-<input name="rebuys" type="number" value="0" class="bg-gray-600 p-3 rounded w-20">
+    <input name="rebuys" type="number" value="0" class="bg-gray-600 p-2 rounded w-20 text-center">
 </td>
+
 <td>
-<input name="addons" type="number" value="0" class="bg-gray-600 p-3 rounded w-20">
+    <input name="addons" type="number" value="0" class="bg-gray-600 p-2 rounded w-20 text-center">
 </td>
+
 <td>
-<input name="winnings" type="number" value="0" class="bg-gray-600 p-3 rounded w-20" required>
+    <input name="winnings" type="number" value="0" class="bg-gray-600 p-2 rounded w-40 text-center">
 </td>
 `
+    table.appendChild(row)
+    updatePositions()
+}
+
+
+function removeLastRow() {
+    let table = document.getElementById("playersTable")
+
+    if (table.rows.length > 0) {
+        table.deleteRow(table.rows.length - 1)
+        updatePositions()
+    }
+}
+
+window.onload = () => {
+    addPlayerRow()
+    addPlayerRow()
+    addPlayerRow()
+    addPlayerRow()
 }
